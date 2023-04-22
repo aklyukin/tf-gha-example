e35b2d2fc0d4942e2cbb980435868a56fa55bf24
aklyukin <klyukin.andrey@gmail.com>
2023-04-23 01:06:20 +0700

    Add fix

--- project/tfcicd.tf
+++ project/tfcicd.tf
@@ -3,12 +3,12 @@
 #############################################################
 
 module "tf_cicd_user" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/1.1.0"
-  delimiter  = var.delimiter
-  name       = var.name
-  namespace  = var.namespace
-  stage      = "prod"
-  tags       = var.tags
+  source    = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/1.1.0"
+  delimiter = var.delimiter
+  name      = var.name
+  namespace = var.namespace
+  stage     = "prod"
+  tags      = var.tags
 
   policy_arns_map = {
     logs = "arn:aws:iam::aws:policy/AdministratorAccess"
